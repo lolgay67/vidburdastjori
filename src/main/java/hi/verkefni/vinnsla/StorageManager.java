84f23abaed685eb3dad6f33ad123fcb95f0267dc
lolgay67 <avg11@hi.is>
2025-04-15 20:55:50 +0000

    added get dates to storage manager

--- src/main/java/hi/verkefni/vinnsla/StorageManager.java
+++ src/main/java/hi/verkefni/vinnsla/StorageManager.java
@@ -2,6 +2,7 @@ package hi.verkefni.vinnsla;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Calendar;
 
 public class StorageManager {
     HashMap<String, ArrayList<Object>> storage = new HashMap<String, ArrayList<Object>>();
@@ -13,5 +14,18 @@ public class StorageManager {
     }
     public Object[] getKeys(){
         return storage.keySet().toArray();
+    }
+    public HashMap<Calendar, String> getDateName(){
+        HashMap<Calendar, String> dateName = new HashMap<Calendar, String>();
+        for (Object names : getKeys()) {
+            ArrayList<Object> tempObjects = storage.get(String.valueOf(names));
+            Calendar tempCalendar = Calendar.getInstance();
+            tempCalendar.set(
+            Integer.valueOf(String.valueOf(tempObjects.get(1))),
+            Integer.valueOf(String.valueOf(tempObjects.get(2))),
+            Integer.valueOf(String.valueOf(tempObjects.get(3))));
+            dateName.put(tempCalendar, String.valueOf(names));
+        }
+        return dateName;
     }
 }
