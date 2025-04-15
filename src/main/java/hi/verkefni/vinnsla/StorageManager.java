b2be1ca2052752a313b7b2949f2c8a77c8b60e88
lolgay67 <avg11@hi.is>
2025-04-15 23:39:44 +0000

    javadoc for storage manager

--- src/main/java/hi/verkefni/vinnsla/StorageManager.java
+++ src/main/java/hi/verkefni/vinnsla/StorageManager.java
@@ -7,18 +7,35 @@ import java.util.Calendar;
 public class StorageManager {
     private HashMap<String, ArrayList<Object>> storage = new HashMap<String, ArrayList<Object>>();
 
+    /**
+     * Stores objects from given array in self
+     * @param objects objects to store
+     */
     public void store(ArrayList<Object> objects) {
         storage.put(String.valueOf(objects.get(0)), objects);
     }
 
+    /**
+     * Gets objects based on string name in first field of stored object
+     * @param name name of object to get
+     * @return arraylist representation of objects stored
+     */
     public ArrayList<Object> getStored(String name) {
         return storage.get(name);
     }
 
+    /**
+     * Getter for all keys/names for getting objects
+     * @return Array representation of keys.
+     */
     public Object[] getKeys() {
         return storage.keySet().toArray();
     }
 
+    /**
+     * Turns all objects into Calendar to string hashmaps so that finding stored objects based on date is easy
+     * @return a calendar-string hashmap of dates-names
+     */
     public HashMap<Calendar, String> getDateName() {
         HashMap<Calendar, String> dateName = new HashMap<Calendar, String>();
         for (Object names : getKeys()) {
@@ -32,7 +49,10 @@ public class StorageManager {
         }
         return dateName;
     }
-
+    /**
+     * removes a stored object based off name
+     * @param name name of object to remove
+     */
     public void removeStored(String name) {
         storage.remove(name);
     }
