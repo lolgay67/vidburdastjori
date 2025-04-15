d4e512d6b3cfee4f017a83dc3db79a6834f63411
lolgay67 <avg11@hi.is>
2025-04-15 21:32:25 +0000

    added license to start of application

---
+++ src/main/java/hi/verkefni/vidmot/licenseController.java
@@ -0,0 +1,12 @@
+package hi.verkefni.vidmot;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class licenseController {
+
+    @FXML public void switchToMain() throws IOException{
+        App.setRoot("eventManager");
+    }
+}
