9242a5550c2e97f6f9988420f29bf4495f382501
lolgay67 <avg11@hi.is>
2025-04-19 23:10:07 +0000

    unit tests for all fucntional parts of the storage manager finished.

--- src/test/java/hi/verkefni/vidmot/storageTest.java
+++ src/test/java/hi/verkefni/vidmot/storageTest.java
@@ -3,24 +3,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
+
 
 import java.util.ArrayList;
 import java.util.function.UnaryOperator;
 
 import hi.verkefni.vinnsla.StorageManager;
-public class Alltests {
+public class storageTest {
     StorageManager storeMan;
+    ArrayList<Object> objects; 
     @Before
     public void setup(){
         storeMan = new StorageManager();
-    }
-
-    @Test
-    public void testStoreAndRetrieve(){
-        ArrayList<Object> objects = new ArrayList<Object>(); 
+        objects = new ArrayList<Object>(); 
         objects.add("EventName");
         objects.add(1234);
         objects.add(1);
@@ -30,17 +27,31 @@ public class Alltests {
         storeMan.store(objects);
         objects.set(0, "Event2");
         storeMan.store(objects);
+    }
+
+    @Test
+    public void getKeysTest(){
         Object[] keyTest = storeMan.getKeys(); // testing the get keys function.
         assertEquals(keyTest.length, 2);
         for (Object object : keyTest) {
             // testing whether all elements in the array are either Event2 or EventName
-            assertTrue(!(object == "Event2"||object=="EventName"));
+            assertTrue((object == "Event2"||object=="EventName"));
         }
+    }
+    @Test
+    public void storeAndRetrieveTest(){
         ArrayList<Object> secondEvent = storeMan.getStored("Event2"); // testing the get stored function.
         for (int i = 0; i < secondEvent.size(); i++) {
             assertEquals(secondEvent.get(i), objects.get(i));
         }
-        assertEquals(0, 1);
     }
+
+    @Test
+    public void deleteTest(){
+        storeMan.removeStored("EventName");
+        Object testNull = storeMan.getStored("EventName");
+        assertNull(testNull);
+    }
+    
     
 }
